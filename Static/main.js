9d6c3d50cd2321bb2898b7c01fbc556146a27ce0
Eric Sherman <ericandrewsherman@gmail.com>
2016-01-20 13:41:56 -0600

    edits

--- Static/main.js
+++ Static/main.js
@@ -9,3 +9,14 @@ var map = L.map('map', {
   layers: [] //layers added here are shown by default
 }).setView([20, 0], 2);
 L.tileLayer('https://a.tiles.mapbox.com/v3/mapbox.world-bright/{z}/{x}/{y}.png').addTo(map);
+
+var key = 'SEQDH9X3sOycBDUzKubGqgzFVOybhdHPgAJrYggu';
+var end = 'http://api.v3.factual.com/t/places?filters='
+var filters = '{"$and":[{"category_labels":{"$includes":"[%5C"AUTOMOTIVE%5C",%5C"MAINTENANCE+AND+REPAIR%5C"]"}}]}&geo={"$circle":{"$center":[41.460460,-81.627303],"$meters":25000}}';
+
+var URL = end + filters + "&KEY=" + key;
+document.write(URL);
+
+var result = $.getJSON(URL);
+success(data,status,xhr)
+document.write(result);
