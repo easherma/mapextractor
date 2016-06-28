def1ef02e3a6c74947640cd0c28095c680f081bf
Jinno Redovan <jredovan.dev@gmail.com>
2016-06-27 19:35:00 -0500

    removed classes.js

--- classes.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  searchParams: function(){
-
-  },
-  API_output: function(object){},
-  looper: function(object){}
-};
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const searchParams = require('../classes.js').searchParams;
 
 const requestify = require('requestify');
 const Factual = require('factual-api'),
       auth = require('../auth.js')
       factual = new Factual(auth.key, auth.secret);
-// const API_output = require('../classes.js').authAPI_outputAPIs;
-// const looper = require('../classes.js').looper;
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-
-  params = {'status':'OK','main':2, 'sub':[], 'user':'default'};
 
   requestify.get('https://api.factual.com/categories?options={"lang":"en","format":"tree"}&KEY=SEQDH9X3sOycBDUzKubGqgzFVOybhdHPgAJrYggu')
           .then((response) => {
@@ -21,7 +16,7 @@ router.get('/', (req, res, next) => {
           })
 });
 
-router.get('/getParams', (req, res, next) => {
+router.post('/getParams', (req, res, next) => {
   //res.render('index');
 });
 
