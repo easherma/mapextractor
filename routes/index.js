8e9e81e9888eb38aef61b2188b0f99eddf3ea6f2
Jinno Redovan <jredovan.dev@gmail.com>
2016-06-27 21:47:06 -0500

    form submit fix

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,15 @@ const Factual = require('factual-api'),
 router.get('/', (req, res, next) => {
 
   requestify.get('https://api.factual.com/categories?options={"lang":"en","format":"tree"}&KEY=SEQDH9X3sOycBDUzKubGqgzFVOybhdHPgAJrYggu')
-          .then((response) => {
+          .then((response) => { //same as function(response) {}
             let resp = response.getBody();
             res.render('index', {categories: JSON.stringify(resp.response.data[0])});
           })
 });
 
 router.post('/getParams', (req, res, next) => {
+  console.log("yeeee");
+  console.log(req.body);
   //res.render('index');
 });
 
