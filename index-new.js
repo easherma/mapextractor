b1003973c5880fb24aaf29ac132f92d8b9791d8c
Jinno Redovan <jredovan.dev@gmail.com>
2016-08-13 15:45:07 -0500

    making progress

--- index-new.js
+++ index-new.js
@@ -4,18 +4,31 @@ var routes = [ { lat: 41.81173, lng: -87.666227},
 var userParams = {main: 23};
 const write = require('./writeToFile.js');
 const mT = require('./MapTasks.js');
+const under = require('underscore');
 
 let run = (route) => {
   let bbox = mT.makeBox(route);
   return mT.getCount(bbox);
 }
 
-//returns promises of
+let runAgain = (bbox) => {
+  let splits = mT.splitBox(bbox);
+  return splits.map((box) => {
+    return mT.getCount(box);
+  });
+}
+
+//returns promises
 let dataProm = routes.map((route) => {
-  let resp = run(route);
-  return resp;
+  let bbox = mT.makeBox(route);
+  return runAgain(bbox);
 });
 
+//runs promises in order
 Promise.all(dataProm).then((data) => {
-  console.log(data);
+  data[0].map((data) => {
+    data.then((data) => {
+      console.log(data.response.total_row_count);
+    });
+  });
 });
