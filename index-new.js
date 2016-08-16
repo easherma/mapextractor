39de72123c889e1e60f9c885ea6d14272183ef02
Jinno Redovan <jredovan.dev@gmail.com>
2016-08-16 17:22:00 -0500

    Completed #8

--- index-new.js
+++ index-new.js
@@ -4,35 +4,55 @@ var routes = [ { lat: 41.81173, lng: -87.666227},
 var userParams = {main: 23};
 const write = require('./writeToFile.js');
 const mT = require('./MapTasks.js');
-const under = require('underscore');
+const _ = require('underscore');
 
 let masterList = [];
 let masterCount = 0;
 
-let run = (bbox) => {
-  let split = mT.splitBox(bbox);
-  Promise.all(split.map((box) => {
-    return mT.getCount(box);
-  })).then((rData) => {
-    rData.map((data) => {
-      if (mT.isWithin(data.response.total_row_count)) {
-        console.log("WITHIN");
-        masterCount+=data.response.total_row_count;
-        console.log("Expect this amount: "+masterCount);
-        //must push the array within the data object
-        masterList.push(data.response.data);
-        console.log("Array amount "+masterList.length);
-        if (masterList.length === masterCount) {
-          console.log("HEY COMPLETE");
-        }
-      } else {
-        console.log("OUTSIDE");
-        run(data.bbox);
-      }
-    });
+/*ALMOST*/
+
+//splits the bbox into 4
+let splitBox = (bbox) => {
+  return mT.splitBox(bbox);
+}
+
+//returns promise
+let getCount = (box) => {
+  return mT.getCount(box);
+}
+
+//decides if it should add to the masterList or call run() again.
+let decide = (data) => {
+  if (mT.isWithin(data.response.total_row_count)) {
+    masterList.push.apply(masterList, createFeatures(data.response.data));
+    if (masterList.length === 875) { //temporary end
+      write("results", mT.featureCollection(masterList));
+    }
+  } else {
+    run(data.bbox);
+  }
+}
+
+//splits the box and checks the count
+let parseSplit = (split) => {
+  split.map((box) => {
+    getCount(box).then((data) => {
+      decide(data);
+    })
   });
 }
 
+//returns array of features
+let createFeatures = (masterList) => {
+  return mT.features(masterList);
+}
+
+//controls the recursion
+let run = (bbox) => {
+  parseSplit(splitBox(bbox));
+}
+
+//runs through each route point
 routes.map((route) => {
   run(mT.makeBox(route));
 });
